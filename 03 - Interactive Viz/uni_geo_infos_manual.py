da2814e024380bf87f7067e9bde82e99141c3839
Romain Ruetschi <romain.ruetschi@gmail.com>
2016-10-28 16:00:49 +0200

    Update uni_geo_info with manual cantons.

---
+++ 03 - Interactive Viz/uni_geo_infos_manual.py
@@ -0,0 +1,99 @@
+
+uni_geo_infos_manual = {
+    'Pädagogische Hochschule Wallis - PHVS': {
+        'canton': 'VS',
+        'locality': 'St-Maurice',
+        'location': {
+            'lat': 46.2155597,
+            'lng': 7.0052943
+        }
+    },
+    'Forschungsanstalten Agroscope - AGS': {
+        'canton': 'VD',
+        'locality': 'Pully',
+        'location': {
+            'lat': 46.512933,
+            'lng': 6.667758
+        }
+    },
+    'Swiss Center for Electronics and Microtech. - CSEM': {
+        'canton': 'NE',
+        'locality': 'Neuchâtel',
+        'location': {
+            'lat': 46.997470,
+            'lng': 6.947437
+        }
+    },
+    'Eidg. Forschungsanstalt für Wald,Schnee,Land - WSL': {
+        'canton': 'ZH',
+        'locality': 'Zürich',
+        'location': {
+            'lat': 47.360478,
+            'lng': 8.454634
+        }
+    },
+    'Forschungsinstitut für Opthalmologie - IRO': {
+        'canton': 'VS',
+        'locality': 'Sion',
+        'location': {
+            'lat': 46.232881,
+            'lng': 7.383033
+        }
+    },
+    'Schweizer Kompetenzzentrum Sozialwissensch. - FORS': {
+        'canton': '',
+        'locality': 'Winterthur',
+        'location': {
+            'lat': 47.501718,
+            'lng': 8.725606}
+    },
+    'Zürcher Fachhochschule (ohne PH) - ZFH': {
+        'canton': 'ZH',
+        'locality': 'Zürich',
+        'location': {
+            'lat': 47.378663,
+            'lng': 8.543272
+        }
+    },
+    'Pädag. Hochschule Tessin (Teilschule SUPSI) - ASP': {
+        'canton': 'TI',
+        'locality': 'Locarno',
+        'location': {
+            'lat': 46.167459,
+            'lng': 8.791478
+        }
+    },
+    'AO Research Institute - AORI': {
+        'canton': 'GR',
+        'locality': 'Davos',
+        'location': {
+            'lat': 46.777395,
+            'lng': 9.813876
+        }
+    },
+    'Physikal.-Meteorolog. Observatorium Davos - PMOD': {
+        'canton': 'GR',
+        'locality': 'Davos',
+        'location': {
+            'lat': 46.813323,
+            'lng': 9.844176
+        }
+    },
+    'Staatsunabh. Theologische Hochschule Basel - STHB': {
+        'canton': 'BS',
+        'locality': '',
+        'location': {
+            'lat': 47.559448,
+            'lng': 7.582678
+        }
+    },
+    'Forschungskommission SAGW': {
+        'canton': 'BE',
+        'locality': 'Bern',
+        'location': {
+            'lat': 46.9470799,
+            'lng': 7.4344473
+        }
+    }
+ }
+
